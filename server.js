d045b0b6c9a606dc5e310112af47a01f76319fd2
notcristi14 <153899201+notcristi14@users.noreply.github.com>
2026-09-25 18:52:13 +0300

    Serve gta-vi-countdown.html for all routes

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
+// Serve gta-vi-countdown.html for root and all region routes
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'gta-vi-countdown.html'));
 });
 
